3e3677e02d31a8bb29fce12a1ace8bc1927b3d61
Sam Khan <psam1625@gmail.com>
2025-03-07 22:13:39 -0500

    1:39:00 - CRUD

--- tutorial/base/views.py
+++ tutorial/base/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 # Create your views here.
-rooms = [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Lets foot foot ball !'},
-    {'id':3, 'name':'fonender learn !'},
-    
 
-]
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 def room(request,pk):
-   return render(request, 'base/room.html')
+   room = Room.objects.get(id=pk)
+   context = {'room': room}
+   return render(request, 'base/room.html', context)
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
