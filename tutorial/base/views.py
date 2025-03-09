7827c0c1a9bb3644e6a9fd686bd087cf33a56dcb
Sam Khan <psam1625@gmail.com>
2025-03-09 00:11:52 -0500

    3:01:25 -  Chat Room Messages CRUD

--- tutorial/base/views.py
+++ tutorial/base/views.py
@@ -8,14 +8,16 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try: 
@@ -30,11 +32,29 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or password does not exist')
 
-    context = {};
+    context = {'page': page,};
     return render(request, "base/login_register.html",context)
+
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    page = 'register'
+    form = UserCreationForm()
+    if request.method =="POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An Error occured during registration')
+    context = {'page': page, 'form': form}
+    return render(request, 'base/login_register.html', context)
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(
@@ -54,6 +74,7 @@ def room(request,pk):
    room = Room.objects.get(id=pk)
    context = {'room': room}
    return render(request, 'base/room.html', context)
+   
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
